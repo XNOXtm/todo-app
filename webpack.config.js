b0f222167c3d902c2f9097a247dccaaa5c2c0e84
XNOXtm <tusharamishra2006@gmail.com>
2026-06-14 21:40:36 +0530

    Config corrections

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
-import path from 'node:path';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(import.meta.dirname, "dist"),
-        clean: true,
-    },
-    devtool: "inline-source-map",
-    devServer: {
-        static: "./dist",
-        watchFiles: ["./src/template.html"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
+  mode: "development",
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+
+  devtool: "eval-source-map",
+
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.html$/i, use: ["html-loader"] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-        ],
-    },
+  },
 };
